8d1be07f1dc742529a0d95f5dc3061a09fe4b086
Math1597 <61025725+Math1597@users.noreply.github.com>
2020-02-14 10:15:48 -0300

    Add files via upload

---
+++ test.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8"/>
+        <title>Documento</title>
+          <style type="text/css">
+    seletor { propriedade: valor; }
+  </style>
+    </head>
+    <body>
+        <b style="color: blue">Olá Mundo</b>
+        <p>Primeiro parágrafo do texto.</p>
+        <p>Segundo parágrafo do texto.</p>
+        <p>Terceiro parágrafo do texto.</p>
+        <img align="right" vspace="35px" hspace="15px" title="Copa América" src="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcRAe5aYILxArGTsqglpn_CriyCECjHEfcgCMi-03MtHcS4fQhQ9" alt="Texto alternativo"/>
+        <a href="Calculator.php">Calculadora</a>
+        <div><a href="#paragrafo3">Clique aqui e ganhe 10 mil reais por mês sem sair de casa!!</a>
+        <!--outros elementos-->
+        <p id="rodape"></p>
+        <table>
+    <thead>
+    <tr>
+        <th>-</th>
+        <th>Bolivia</th>
+        <th>Brasil</th>
+        <th>Uruguai</th>
+        <th>Venezuela</th>
+    </tr>
+    </thead>
+    <tbody>
+    <tr>
+        <th>BOL</th>
+        <td>-</td>
+        <td>0-0</td>
+        <td>3-2</td>
+        <td>2-1</td>
+    </tr>
+    <tr>
+        <th>BRA</th>
+        <td>5-3</td>
+        <td>-</td>
+        <td>3-1</td>
+        <td>2-1</td>
+    </tr>
+    <tr>
+        <th>URU</th>
+        <td>4-0</td>
+        <td>1-1</td>
+        <td>-</td>
+        <td>0-1</td>
+    </tr>
+    <tr>
+        <th>VEN</th>
+        <td>5-0</td>
+        <td>0-2</td>
+        <td>1-1</td>
+        <td>-</td>
+    </tr>
+    </tbody>
+        </table>
+        </body>
+        </html>
+
